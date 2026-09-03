97bfff3159d389568e97c427589b0704ce4ef1e5
IronGPS2026 <ceo@iron-gps.com>
2026-09-02 20:24:24 -0500

    borrar archivos anteriores

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import createMiddleware from 'next-intl/middleware';
-import { routing } from './i18n/routing';
-
-export default createMiddleware(routing);
-
-export const config = {
-  // Aplica el middleware a todas las rutas excepto assets internos y estáticos.
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
-};
